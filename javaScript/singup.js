7818761175390bad27db39de57cf718a3fbd38b3
Ayazalidharejo <ayazali.xwave@gmail.com>
2024-10-12 14:35:54 +0500

    local storege

---
+++ javaScript/singup.js
@@ -0,0 +1,16 @@
+document.getElementById('signupForm').addEventListener('submit', function(eve) {
+    eve.preventDefault();
+  
+    let Email = document.getElementById('userEmail').value;
+    let Password = document.getElementById('userPassword').value;
+    console.log(Email, Password);
+  
+    if (Email && Password) {
+      localStorage.setItem('Email', Email);
+      localStorage.setItem('Password', Password);
+  
+      window.location.href = "../login/login.html";
+      alert('Registered completely');
+    }
+  });
+  
